18281ddc1cd0333c8d79e530d5df275fc33b3485
dev-miguel-resources <ingenieromiguelch@gmail.com>
2022-03-29 20:25:13 -0300

    register component ok

--- src/pages/auth/Register.js
+++ src/pages/auth/Register.js
@@ -1,23 +1,36 @@
 import React, { useState, useEffect } from "react";
-import { auth } from "../../firebase"; 
+import { auth } from "../../firebase";
 import { toast } from "react-toastify";
 import { useSelector } from "react-redux";
 
 const Register = ({ history }) => {
-  const [email, setEmail] = useState(""); 
+  const [email, setEmail] = useState("");
   const { user } = useSelector((state) => ({ ...state }));
 
   useEffect(() => {
+    if (user && user.token) history.push("/");
+  }, [history, user]);
 
-  }, []);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const config = {
+      url: process.env.REACT_APP_REGISTER_REDIRECT_URL,
+      handleCodeInApp: true,
+    };
 
-  const handleSubmit = () => {
-
+    await auth.sendSignInLinkToEmail(email, config);
+    toast.success(
+      `Email is sent to ${email}. Click the link to complete your registration.`
+    );
+    // save user to localstorage
+    window.localStorage.setItem("emailForRegistration", email);
+    // clear state
+    setEmail("");
   };
 
   const registerForm = () => (
     <form onSubmit={handleSubmit}>
-      <input 
+      <input
         type="email"
         className="form-control"
         value={email}
@@ -25,20 +38,24 @@ const Register = ({ history }) => {
         placeholder="Your email"
         autoFocus
       />
+
+      <br />
+      <button type="submit" className="btn btn-raised">
+        Register
+      </button>
     </form>
   );
 
   return (
     <div className="container p-5">
       <div className="row">
-        <div className="col md-6 offset-md-3">
+        <div className="col-md-6 offset-md-3">
           <h4>Register</h4>
           {registerForm()}
         </div>
       </div>
     </div>
   );
-
 };
 
 export default Register;
